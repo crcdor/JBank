518c1a73bb248c78236c00e1b86e7e42677f7626
crcdor <rudynurhadi@gmail.com>
2016-02-18 16:36:44 +0700

    modul 1

---
+++ Bank.java
@@ -0,0 +1,92 @@
+
+/**
+ * Write a description of class Bank here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bank {
+    // instance variables - replace the example below with your own
+    private static double creditInterestRate;
+    private static String closeTime;
+    private static double investmentInterestRate;
+    private static int lastCustID;
+    private static int nextCustID;
+    private static String phone;
+    private static double premiumInterestRate;
+    private static String startTime;
+    public static String website;
+    public static String bankAddress = "1234 JavaStreet, AnyCity, ThisState, 34567";
+    public static int maxNumOfCustomers = 20;
+    public static String bankName = "JBANK";
+
+    /**
+     * Constructor for objects of class Bank
+     */
+    public Bank() {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public static String getAddress() {
+        return null;
+    }
+    
+    public static double getCreditRate() {
+        return 0;
+    }
+    
+    public static double getInvestmentRate() {
+        return 0;
+    }
+    
+    public static String getHoursOfOperation() {
+        return null;
+    }
+    
+    public static int getLastID() {
+        return 0;
+    }
+    
+    public static int getMaxCustomers() {
+        return 0;
+    }
+    
+    public static String getName() {
+        return null;
+    }
+    
+    public static int getNextID() {
+        return 0;
+    }
+    
+    public static String getWebsite() {
+        return null;
+    }
+    
+    public static double getPremiumRate() {
+        return 0;
+    }
+    
+    public static String getPhone() {
+        return null;
+    }
+    
+    public static void setCreditRate(double rate) {
+        
+    }
+    
+    public static void setInvestmentRate(double rate) {
+        
+    }
+    
+    public static void setPremium(double rate) {
+        
+    }
+}
---
+++ Teller.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Teller here.
+ * 
+ * @author Rudy Nurhadi 
+ * @version 18/02/2016
+ */
+public class Teller {
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Teller
+     */
+    public Teller() {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void main(String[] args) {
+        // put your code here
+        
+    }
+}
